9ec07ee7b43c8b1f4d9354025544e6aa9fead6de
Marcos Antônio dos Santos <marcaosi2014@gmail.com>
2016-10-04 16:01:29 -0300

    Corrigir fontes

--- index.php
+++ index.php
@@ -36,12 +36,15 @@
     		<div class="col-xs-12 col-sm-4">
     			<a href="<?=$urlBase ?>"><img class="img-responsive" src="<?= $urlBase ?>img/Logo.png" alt="Logo Menu" /></a>
     		</div>
-    		<div class="col-xs-12 col-sm-8">
+    		<div class="hidden-xs col-sm-8">
     			<nav class="navbar navbar-default">
 				  <div class="container-fluid">
 				    <!-- Brand and toggle get grouped for better mobile display -->
-				    <div class="navbar-header">
-				      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				    <div class="navbar-header text-center">
+                        <p class="text-center brand-menu visible-xs">
+                            Menu
+                        </p>
+				      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2" aria-expanded="false">
 				        <span class="sr-only">Toggle navigation</span>
 				        <span class="icon-bar"></span>
 				        <span class="icon-bar"></span>
@@ -50,7 +53,7 @@
 				    </div>
 
 				    <!-- Collect the nav links, forms, and other content for toggling -->
-				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
 				      <ul class="nav navbar-nav">
 				        <li><a href="#" class="active-link-menu">Home</a></li>
 				        <li class="dropdown">
@@ -98,6 +101,71 @@
 				</nav>
     		</div>
     	</div>
+    </div>
+
+    <div class="container-fluid menu-xs visible-xs">
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header text-center">
+                <p class="text-center brand-menu visible-xs">
+                    Menu
+                </p>
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li><a href="#" class="active-link-menu">Home</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">O Clube <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Diretoria</a></li>
+                    <li><a target="_blank" href="<?=$urlBase ?>docs/Estatuto.pdf">Estatuto</a></li>
+                    <li><a href="#">História</a></li>
+                    <li><a target="_blank" href="<?=$urlBase ?>docs/CodigoDisciplinar.pdf">Código Disciplinar</a></li>
+                    <li><a href="#">Diretrizes</a></li>
+                  </ul>
+                </li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Estrutura <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="<?=$urlBase ?>estrutura/academia">Academia</a></li>
+                    <li><a href="<?=$urlBase ?>estrutura/playground">Playground</a></li>
+                    <li><a href="<?=$urlBase ?>estrutura/campos">Campos de Futebos</a></li>
+                    <li><a href="<?=$urlBase ?>estrutura/churrasqueiras">Churrasqueria</a></li>
+                    <li><a href="<?=$urlBase ?>estrutura/ginasio">Ginásio</a></li>
+                    <li><a href="<?=$urlBase ?>estrutura/parqueAquatico">Parque Aquático</a></li>
+                    <li><a href="<?=$urlBase ?>estrutura/salaoSocial">Salão Social</a></li>
+                    <li><a href="<?=$urlBase ?>estrutura/sauna">Sauna</a></li>
+                    <li><a href="<?=$urlBase ?>estrutura/vestiarios">Vestiários</a></li>
+                  </ul>
+                </li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Atividades <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Bocha</a></li>
+                    <li><a href="#">Futebol</a></li>
+                    <li><a href="#">Tênis</a></li>
+                    <li><a href="#">Muay Thai</a></li>
+                    <li><a href="#">Pilates</a></li>
+                    <li><a href="#">Judô</a></li>
+                    <li><a href="#">Zumba</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Eventos</a></li>
+                <li><a href="#">Fotos</a></li>
+                <li><a href="#">Contato</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
     </div>
 
     <div class="container-fluid">
