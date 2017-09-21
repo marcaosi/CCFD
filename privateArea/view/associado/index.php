14eef2bee4aeccb7efffcfaeb96372635382b9fd
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2017-09-21 01:22:45 -0300

    Adicionada tela para mostrar documentos privados

--- privateArea/view/associado/index.php
+++ privateArea/view/associado/index.php
@@ -1,3 +1,26 @@
 <?php
 include_once('../../service/autorizacaoAssociadoService.php');
 
+include_once('../../../includes/cabecalho_ccfd.php');
+
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-8 col-xs-offset-2">
+            <h3 class="title-page-inside">Documentos Privados</h3><br>
+
+            <ul>
+                <li><a href="">Documento 1</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+
+echo "<br><br><br><br><br><Br><br><Br><Br><Br><br><br><br><br>";        
+
+include_once('../../../includes/rodape_ccfd.php');
